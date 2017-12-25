066cb2b16efd42ac1d7d17afd881fe8de2a228b2
duwei <duwei1@bis.com.cn>
2017-12-25 10:15:22 +0800

    duweix@126.com

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,13 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	entry: {
-		app: './src/index.js'
-	},
-	devtool: "inline-source-map",
-	devServer: {
-		contentBase: "./dist",
-		hot: true
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			}
-		]
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
 	},
 	plugins: [
-		new CleanWebpackPlugin(["dist"]),
-		new HtmlWebpackPlugin({title: "Ht Module Replacement"}),
-		new webpack.NamedModulesPlugin(),
-		new webpack.HotModuleReplacementPlugin()
-	],
-	output: {
-		filename: "[name].bundle.js",
-		path: path.resolve(__dirname, "dist"),
-		publicPath: "/"
-	}
+		new UglifyJSPlugin()
+	]
 };
